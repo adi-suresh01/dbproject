72e7bcf6921851eb9f57de0f3f5d69ca85a7d0b2
adi-suresh01 <152752311+adi-suresh01@users.noreply.github.com>
2023-12-06 11:31:16 -0500

    Update sqlconnect.py

--- myDB/myDB/sqlconnect.py
+++ myDB/myDB/sqlconnect.py
@@ -3,8 +3,15 @@
 import mysql.connector
 
 # Establishing a connection to the local host by entering a valid username and password
-dataBase = mysql.connector.connect(
+
+database = mysql.connector.connect(
   host="localhost",
   user="root",
   passwd="password123"
 )
+
+# preparing a cursor object
+cursor = database.cursor()
+
+# creating database
+cursor.execute("CREATE DATABASE BANK2")
