3253ed6f99b44c4037220a4e36e5045e926f907e
adi-suresh01 <152752311+adi-suresh01@users.noreply.github.com>
2023-12-06 17:47:51 -0500

    Update sqlconnect.py

--- myDB/myDB/sqlconnect.py
+++ myDB/myDB/sqlconnect.py
@@ -32,7 +32,7 @@ cursor = database.cursor()
 #cursor.execute("CREATE TABLE ISSUES (index2 int PRIMARY KEY, coupon_no double, branch_no double, FOREIGN KEY (coupon_no) REFERENCES BOND(coupon_no), FOREIGN KEY (branch_no) REFERENCES BANK(branch_no))")
 
 
-cursor.execute("UPDATE TRANSACTION SET from_acc = '9090-3282-6637-0732' WHERE from_acc = '6314-2668-9487-0709'")
+cursor.execute("UPDATE TRANSACTION SET from_acc = '4432-4474-5427-9937' WHERE from_acc = '1369-8067-8282-0015'")
 
 
 database.commit()
